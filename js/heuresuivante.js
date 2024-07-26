85d1a892998bcd3f3f5bccc79e65c6cab78642c4
Jordan Ricca <jordan.ricca@lepoles.org>
2024-07-26 09:53:21 +0200

    heure

---
+++ js/heuresuivante.js
@@ -0,0 +1,8 @@
+let heure = prompt("Choisissez l'heure");
+let minute = prompt("Choisissez la minute");
+let seconde = parseInt(prompt("Choisissez la seconde"));
+
+
+seconde = seconde + 1;
+alert ( heure + " H " + minute + " M " + seconde + " S voici le resultat 1 seconde plus tard" );
+
